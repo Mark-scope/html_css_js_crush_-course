c084e928a5484890452964d0695271647fcb16b6
Mark-scope <agyemangmark1@gmail.com>
2021-03-05 10:36:03 +0000

    made some changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,45 @@
 console.log("hellooo....")
+
 console.log("cars")
+
 let d=78
+
 console.log(d)
+
+// variables
+let m="Mark"
+
+console.log(m)
+let a=5
+let b=9
+console.log(a+b);
+console.log(a*b)
+
+let surname = "JOJO "
+let firstname = "JOE"
+console.log(surname+firstname)
+
+
+
+// Arrays and Object 
+// Arrays (list of items or names)
+let myarray = [6,4,10,3,4,5,6]
+let names = [ "kofi", "kwasi", "amma"]
+let mymixed = [ 0,4,6,"you","yam","banana"]
+
+// the first element's index is zero.0 
+console.log(myarray[2]);
+console.log(myarray[3]);
+console.log()
+
+// take somethig off use myarray.pop()
+myarray.push(50)
+console.log(myarray);
+// use myarray.splice ... to remove a specific range
+
+// Objects (one thing with several properties)
+let myobject = {name:"Kofi", classname:"Marry Gold", results[
+    {subject:"English", mark:40, position:1},
+    {subject:"Maths", mark:80, position:4},
+    {subject:"Social", mark:50, position:3},
+]
